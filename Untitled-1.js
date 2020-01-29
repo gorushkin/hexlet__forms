8e24f50b568cb54a5acaf421b68d67c8c50e386d
gorushkin <gorushkin@gmail.com>
2020-01-29 07:40:04 +0300

    work

---
+++ Untitled-1.js
@@ -0,0 +1,46 @@
+    // if (state.form.password.length >= 6 && state.form.password === state.form.passwordConfirmation) {
+    //   state.form.password = 'valid'
+    //   console.log('pass is correct');
+    // };
+    // if (state.form.password.length >= 6) {
+    //   console.log('pass is long!!!');
+    // } else {
+    //   console.log('pass is short!!!');
+    // }
+
+    const state = {
+      form: {
+        formState: 'invalid',
+        name: null,
+        nameState: 'invalid',
+        email: null,
+        emailState: 'invalid',
+        password: null,
+        passState: 'invalid',
+        passwordConfirmation: null,
+      }
+    }
+  
+    const btn = document.querySelector('[type="submit"]');
+  
+    watch(state, 'form', () => {
+      if (isEmailValid(state.form.email)) {
+        console.log(state.form.email);
+        state.form.emailState = 'valid'
+        // console.log('email is valid');
+      } else {
+        // console.log('email is not valid');
+      };
+    });
+  
+    const form = document.querySelector('[data-form="sign-up"]');
+  
+    const elements = document.querySelectorAll('[id^="sign-up"]');
+    elements.forEach(element => {
+      element.addEventListener('change', (e) => {
+        const value = e.target.value;
+        const name = e.target.name
+        state.form[name] = value;
+      })
+    })
+  }
